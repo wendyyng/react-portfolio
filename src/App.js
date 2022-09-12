51c7423983ebe2e25771e73326311d402cf0d45d
wendyyng <ng_yih_wei@hotmail.com>
2022-09-12 12:36:34 -0700

    Add website icon and logo to navbar

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar';
 function App() {
   
   return (
-    <VStack  pt={7}>
+    <VStack  pt={2}>
         <Navbar></Navbar>
         <Header></Header>
         <About></About>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link, Flex, VStack, Heading } from "@chakra-ui/layout"
-import { ColorModeContext, IconButton, useColorMode, Spacer } from '@chakra-ui/react';
+import { Link, Flex, VStack, Heading  } from "@chakra-ui/layout"
+import { ColorModeContext, IconButton, useColorMode, Spacer, Image, Box  } from '@chakra-ui/react';
 import { FaSun, FaMoon, FaGithub, FaLinkedin} from 'react-icons/fa'
 
 
@@ -10,9 +10,10 @@ function Navbar() {
     const isDark = colorMode === "dark"
 
   return (
-    <Flex w="100%">
+    <Flex w="100%" justify="center" align="center">
     <Heading ml="8" size="md" color="pink.400">
-      Wendy Ng
+            <Image src='/android-chrome-512x512-removebg-pink.png' alt='logo' boxSize='80px'
+    objectFit='cover'/>
     </Heading>
     <Spacer></Spacer>
     <Link id="icons" href='https://www.linkedin.com/in/wendyywng/' isExternal>

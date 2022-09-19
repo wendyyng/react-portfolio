1a33774f7d37366140e51214153ba45e4c2ced9d
wendyyng <ng_yih_wei@hotmail.com>
2022-09-18 23:58:09 -0700

    Hide my resume button

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,15 +12,15 @@ function Header() {
         bg: "#001845",
       }} justifyContent="center" pb="10">
         <Flex direction={isNotSmallerScreen ? "row" : "column"} spacing="200px" p={isNotSmallerScreen ? "32" : "0"} alignSelf="flex-start">
-            <Box mt={isNotSmallerScreen ? "0" : 16 } align="flex-start" spacing={3}>
+            <Box mt={isNotSmallerScreen ? "0" : 16 } align="flex-start" spacing={3} mb="8" >
                 <Text fontSize="2xl" fontWeight="bold">Hi, My name is</Text>
                 <Text fontSize="7xl" fontWeight="bold" bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' >Wendy Ng</Text>
                 <Text fontSize="3xl" color={isDark ? "gray.200" : "gray.600"}>I am a web developer</Text>
-            <Button p={7}  mt={7} colorScheme="pink"   _hover={{
+            {/* <Button p={7}  mt={7} colorScheme="pink"   _hover={{
                 bgGradient: "linear(to-r, blue.200, pink.200)",
               }}onClick={() =>
                         window.open("https://www.dropbox.com/s/tje6qp0lzqo1mvq/Ng%2C%20Wendy-Resume.pdf?dl=0")
-                    }>My Resume </Button>
+                    }>My Resume </Button> */}
             </Box>
         </Flex>
     </Flex>

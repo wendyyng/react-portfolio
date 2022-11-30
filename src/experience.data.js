1256c5d236e662e6b6a7eb34d9cf918bc21ad13d
wendyyng <ng_yih_wei@hotmail.com>
2022-11-29 17:30:24 -0800

    Add Experience section and data file and update Chakra UI

---
+++ src/experience.data.js
@@ -0,0 +1,28 @@
+export const experience = [
+  {
+    position_title: "Open Source Software Developer",
+    company: "OpenTree Education",
+    date: "Oct 2022 - Present",
+    description: [
+      "Worked in an agile team and contributed to the Learning Management System ‘The Rhizone’ (TypeScript, React, Node.js, Express, Material UI, MySQL, Docker) by integrating the full calendar feature",
+      "Implemented the UI of the calendar feature and was responsible for the communication of data with the backend through an API",
+      "Wrote unit tests for the backend using Jest to ensure the functionality and scalability of the backend codebase",
+      "Completed tickets and reviewed pull requests in a timely manner that allowed the team to complete the feature ahead of schedule",
+      "Developed a product mindset by writing the user stories and product features in the product specification",
+    ],
+    github_link: "https://github.com/OpenTree-Education/rhizone-lms",
+    company_logo: "https://avatars.githubusercontent.com/u/84355280?s=200&v=4",
+  },
+  {
+    position_title: "Open Source Contributor",
+    company: "Multiplied By One Org | Trauma and Dissociative Disorders",
+    date: "Oct 2022 - Present",
+    description: [
+      "Contributed to the Multiplied By One App (React, Material UI, Firebase, Storybook) by developing the System Map Profile page and creating the reusable profile card components based on the UI design guideline",
+      "Reviewed the pull requests created by other contributors by testing new features, pointing out errors and providing suggestions",
+      "Created the contributing guideline and templates for pull requests and issues to help the project to increase successful contributions",
+    ],
+    github_link: "https://github.com/Multiplied-By-One/MBO-App/",
+    company_logo: "https://avatars.githubusercontent.com/u/76632444?s=200&v=4",
+  },
+];

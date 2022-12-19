dcb63a40ad2b253d702139d84c165f5d44df66f0
wendyyng <ng_yih_wei@hotmail.com>
2022-12-19 11:34:55 -0800

    Edit experience data

--- src/data/experience.data.js
+++ src/data/experience.data.js
@@ -1,8 +1,19 @@
 export const experience = [
   {
-    position_title: "Open Source Software Developer",
+    position_title: "Web Developer (Volunteer)",
+    company: "Society for Canadian Women in Science and Technology",
+    date: "November 2022 - Present",
+    description: [
+      "Created mock-ups and designed the layout and flow of the 'ms infinity' program pages using Figma",
+      "Consulted with Youth Engagement Director to gather and define the organization's requirements for the website revamp"
+    ],
+    company_logo:
+      "https://user-images.githubusercontent.com/71687298/208505498-a6fad389-d70b-41cb-b989-932835e5804b.jpg",
+  },
+  {
+    position_title: "Open Source Software Developer / Mentee",
     company: "OpenTree Education",
-    date: "Oct 2022 - Present",
+    date: "Oct - Dec 2022",
     description: [
       "Worked in an agile team and contributed to the Learning Management System ‘The Rhizone’ (TypeScript, React, Node.js, Express, Material UI, MySQL, Docker) by integrating the full calendar feature",
       "Implemented the UI of the calendar feature and was responsible for the communication of data with the backend through an API",
@@ -17,7 +28,7 @@ export const experience = [
   {
     position_title: "Open Source Contributor",
     company: "Multiplied By One Org | Trauma and Dissociative Disorders",
-    date: "Oct 2022 - Present",
+    date: "Oct - Dec 2022",
     description: [
       "Contributed to the Multiplied By One App (React, Material UI, Firebase, Storybook) by developing the System Map Profile page and creating the reusable profile card components based on the UI design guideline",
       "Reviewed the pull requests created by other contributors by testing new features, pointing out errors and providing suggestions",

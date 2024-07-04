eb76042fe99ec8cd5e417985e99679ce2f8f8982
wendyyng <ng_yih_wei@hotmail.com>
2024-07-04 18:21:58 +0800

    Update experience data

--- src/data/experience.data.js
+++ src/data/experience.data.js
@@ -1,5 +1,21 @@
 export const experience = [
   {
+    position_title: "Frontend Developer",
+    company: "PlaceSpeak",
+    date: "May 2023 - Jan 2024",
+    description: [
+      "Led the homepage redesign project to enhance user experience for regular users",
+      "Implemented 'Select Electoral District' feature on the Placeit map as per client request for Elections Yukon's Consulatation Topic",
+      "Focused on UI/UX improvements and bug fixes for various pages including Consultation Topic, Neighborhood, and About pages",
+      "Navigated challenges with legacy codebase, utilizing jQuery and React Class Components",
+      "Collaborated closely with team members, contributing technical expertise and driving improvements",
+    ],
+    skills: "JavaScript, React, Redux, Node.js, Python, Django, PostgreSQL, Figma",
+    website: "https://placespeak.com/",
+    company_logo:
+      "https://private-user-images.githubusercontent.com/71687298/345793626-ddf7ff99-f78e-4b78-bb0d-64c3d5833946.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjAwODg0NjAsIm5iZiI6MTcyMDA4ODE2MCwicGF0aCI6Ii83MTY4NzI5OC8zNDU3OTM2MjYtZGRmN2ZmOTktZjc4ZS00Yjc4LWJiMGQtNjRjM2Q1ODMzOTQ2LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MDQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzA0VDEwMTYwMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTY1YmRkMTJkZGRiNTg4MzAyZTFiNjFlZDBhYjdkODdiMjNkOGY0NmFlNmM5ODFlM2ZmOTk3YmYwOWU2ZjRkYTMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.eOpqJRkED1e__ZNNQyfSk8JDrCGshAsLK88qWUdyO_I", 
+  },
+  {
     position_title: "Web Developer",
     company: "Society for Canadian Women in Science and Technology",
     date: "Oct 2022 - Feb 2023",
@@ -25,19 +41,5 @@ export const experience = [
     skills: "TypeScript, React, Node.js, Express, Material UI, MySQL, Docker",
     company_logo:
       "https://user-images.githubusercontent.com/71687298/204715984-926cf6ce-fc8c-4d75-90b2-11aa719c6f7a.png",
-  },
-  {
-    position_title: "Frontend Developer",
-    company: "Multiplied By One Org | Trauma and Dissociative Disorders",
-    date: "Oct - Dec 2022",
-    description: [
-      "Contributed to Multiplied By One App by developing System Map Profile page and reusable profile card components based on UI design guidelines",
-      "Reviewed pull requests created by other contributors by testing new features, pointing out errors and providing suggestions",
-      "Created contributing guideline and templates for pull requests and issues to help the project to increase successful contributions",
-    ],
-    github_link: "https://github.com/Multiplied-By-One/MBO-App/",
-    skills: "React, Material UI, Firebase",
-    company_logo:
-      "https://user-images.githubusercontent.com/71687298/204715995-2f452acc-0e08-4d53-b6a0-5d3d1022be06.png",
-  },
+  }
 ];

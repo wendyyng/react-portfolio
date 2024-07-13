a9ca135cff554cacf4baadd105e370823154eb10
wendyyng <ng_yih_wei@hotmail.com>
2024-07-13 17:27:23 +0800

    Adjust chatbox responsiveness and icon colour when chatbox is closed

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { Input, Button, Box, Text, VStack, HStack, Alert, AlertIcon, Avatar, Flex  } from '@chakra-ui/react';
-import { FaRobot, FaPaperPlane, FaTimes } from 'react-icons/fa';
+import { Input, Button, Box, Text, VStack, HStack, Alert, AlertIcon, Avatar, Flex, Center } from '@chakra-ui/react';
+import { FaRobot, FaPaperPlane, FaTimes, FaComment } from 'react-icons/fa';
 
 const MAX_CHAR_LENGTH = 100;
 
@@ -9,12 +9,12 @@ function Chat() {
     const [message, setMessage] = useState('');
     const [chatHistory, setChatHistory] = useState([]);
     const [error, setError] = useState('');
-    const [isOpen, setIsOpen] = useState(true);
+    const [isMinimized, setIsMinimized] = useState(false);
     const chatContainerRef = useRef(null);
 
     const sendMessage = async () => {
-         if (!message) return; // Don't send empty messages
-         console.log(message)
+        if (!message) return; // Don't send empty messages
+        console.log(message);
         if (message.length > MAX_CHAR_LENGTH) {
             setError(`Message cannot exceed ${MAX_CHAR_LENGTH} characters.`);
             return;
@@ -37,27 +37,50 @@ function Chat() {
         } catch (error) {
             console.error('Error:', error);
             const errorMessage = { role: 'bot', content: 'An error occurred. Please try again.' };
-             setChatHistory(prevChatHistory => [...prevChatHistory, userMessage, errorMessage]);
+            setChatHistory(prevChatHistory => [...prevChatHistory, userMessage, errorMessage]);
         }
     };
 
-    const handleClose = () => {
-        setIsOpen(false); // Close the chatbox
+    const toggleChatbox = () => {
+        setIsMinimized(prev => !prev);
     };
 
     useEffect(() => {
-    // Scroll to the bottom whenever chatHistory changes
-    if (chatContainerRef.current) {
-        chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
-    }
+        // Scroll to the bottom whenever chatHistory changes
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
     }, [chatHistory]);
 
-    if (!isOpen) {
-        return null; // Render nothing if chatbox is closed
+    if (isMinimized) {
+        return (
+           <Box
+                w="4rem"
+                h="4rem"
+                position="fixed"
+                bottom={4}
+                right={4}
+                bg="white"
+                p={3}
+                borderRadius="xl"
+                boxShadow="md"
+                zIndex={10}
+                cursor="pointer"
+                onClick={toggleChatbox}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Center>
+                    <FaComment size={30} color="#FED7E2" />
+                </Center>
+            </Box>
+        );
     }
 
     return (
-         <Box
+        <Box
+            ml={10}
             p={4}
             position="fixed"
             bottom={4}
@@ -66,9 +89,14 @@ function Chat() {
             boxShadow="md"
             bg="white"
             zIndex={10}
+            width={{ base: '100%', sm: '80%', md: '70%', lg: '50%' }}
+            maxW="630px"
+            // left="0.2rem"
         >
-           <Box ml={2} >
-                <FaTimes size={20} color="#CBD5E0" cursor="pointer" onClick={handleClose}/>
+            <Box ml={2} display="flex" justifyContent="flex-end">
+                <HStack >
+                    <FaTimes size={20} color="#CBD5E0" cursor="pointer" onClick={toggleChatbox} />
+                </HStack>
             </Box>
             <Box
                 mt={4}
@@ -76,14 +104,14 @@ function Chat() {
                 mb={1}
                 borderRadius="xl"
                 h="200px"
-                w='600px'
+                // w='600px'
                 overflowY="scroll" // Enable vertical scrolling
                 ref={chatContainerRef} // Reference to the chat container
                 border="1px solid #CBD5E0"
             >
                 <VStack spacing={4} mt={4}>
                     {chatHistory.map((msg, index) => (
-                       <HStack
+                        <HStack
                             key={index}
                             alignSelf={msg.role === 'user' ? 'flex-end' : 'flex-start'}
                             bg={msg.role === 'user' ? 'blue.100' : 'pink.100'}
@@ -117,6 +145,7 @@ function Chat() {
                     bgGradient="linear(to-l, #7928CA, #FF0080)"
                     px={3}
                     py={3}
+                    ml={1}
                     fontWeight="semibold"
                     rounded="full"
                     _hover={{
